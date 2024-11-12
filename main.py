ec926fabccab8a041dadca49412100348d259267
PedroPaulo-Git <pedropaulopyter6@gmail.com>
2024-11-12 08:03:45 -0300

    creating routes

--- main.py
+++ main.py
@@ -5,6 +5,6 @@ from routes.client import client_route
 app = Flask(__name__)
 
 app.register_blueprint(home_route)
-app.register_blueprint(client_route)
+app.register_blueprint(client_route,url_prefix='/clients')
 
 app.run(debug=True)
--- routes/client.py
+++ routes/client.py
@@ -4,13 +4,55 @@ client_route = Blueprint('client_route',__name__)
 """
 client routes
     
-    - /clients/
+    - /clients/(GET)
+     - /clients/(POST)
+     - /clients/new - render form to create c;ient
+     - /clients/<id> - get once client by id
+      - /clients/<id>/edit - 
+       - /clients/<id>/update (PUT)
+        - /clients/<id>/delete -(DELETE) delete client
 """
 @client_route.route('/')
 def client_list():
-    return render_template('teste.html')
+    return {'page':'client list'}
+
+
+
+@client_route.route('/',methods=['POST'])
+#insert on db
+def client_insert():
+    return {'page':'client insert'}
+
+
+
+@client_route.route('/new',methods=['GET'])
+#create client
+def client_create():
+    return {'page':'client create'}
+
 
 @client_route.route('/<int:client_id>')
-def client_once(client_id):
-    # print(client_id)
-    return render_template('teste.html')
+#show client info
+def client_details():
+    return {'page':'client details'}
+
+
+
+@client_route.route('/<int:client_id>/edit')
+#edit form client
+def client_edit():
+    return {'page':'client edit'}
+
+
+@client_route.route('/<int:client_id>/update',methods=['PUT'])
+#update client info
+def client_update():
+    return {'page':'client update'}
+
+
+@client_route.route('/<int:client_id>/delete')
+#delete client
+def client_delete():
+    return {'page':'client delete'}
+
+
